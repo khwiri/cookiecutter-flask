99d2c4924b92690e1bb673260a694a77435b8073
khwiri <5530052+khwiri@users.noreply.github.com>
2021-02-06 21:23:31 -0500

    :recycle: Switch post_gen_project Hook to pathlib

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,12 @@
-import os
-from functools import partial
+from pathlib import Path
+
+
+PROJECT_ROOT = Path('.')
 
 
 if __name__ == '__main__':
-    instance_config_directory = os.path.join(os.path.abspath('.'), 'instance')
-    instance_config_directory_exists = partial(os.path.exists, instance_config_directory)
-    if not instance_config_directory_exists():
-        os.mkdir(instance_config_directory)
-        if not instance_config_directory_exists():
-            raise FileNotFoundError(f'Failed while creating instance configuration directory ({instance_config_directory}).')
-    else:
-        print('Skip creating instance configuration directory because it already exists.')
-
-    instance_config = os.path.join(instance_config_directory, 'config.py')
-    if not os.path.exists(instance_config):
-        with open(instance_config, 'wt') as f:
-            f.write('# Place secret configuration like database passwords and api keys here...\n')
-    else:
-        print('Skip creating instance configuration because it already exists.')
+    # Create instance configuration placeholder
+    instance_path = PROJECT_ROOT / 'instance'
+    instance_path.mkdir(exist_ok=True)
+    with (instance_path / 'config.py').open(mode='w') as f:
+        f.write('# Place secret configuration like database passwords and api keys here...\n')
