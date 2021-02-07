8b3c19dff29b571626096c97562282307cb95d0a
khwiri <5530052+khwiri@users.noreply.github.com>
2021-02-07 11:46:14 -0500

    :bug: Ensure Line Endings During Template Rendering
    The post_gen_project hook has been updated to ensure line
    endings during template rendering. This is required for
    certain files when rendering on Windows.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,21 @@
+from typing import Iterable
 from pathlib import Path
+from pathlib import PosixPath
 
 
 PROJECT_ROOT = Path('.')
+
+
+def lf_line_endings_required_files() -> Iterable[PosixPath]:
+    yield PROJECT_ROOT / '.python-version'
+    yield PROJECT_ROOT / '.nvmrc'
+    yield from PROJECT_ROOT.glob('**/*.sh')
+
+
+def translate_to_lf_line_endings(file :PosixPath) -> None:
+    file_bytes_original_line_endings = file.read_bytes()
+    file_bytes_lf_line_endings = file_bytes_original_line_endings.replace(b'\r\n', b'\n')
+    file.write_bytes(file_bytes_lf_line_endings)
 
 
 if __name__ == '__main__':
@@ -10,3 +24,7 @@ if __name__ == '__main__':
     instance_path.mkdir(exist_ok=True)
     with (instance_path / 'config.py').open(mode='w') as f:
         f.write('# Place secret configuration like database passwords and api keys here...\n')
+
+    # Ensure line endings for certain files
+    for f in lf_line_endings_required_files():
+        translate_to_lf_line_endings(f)
