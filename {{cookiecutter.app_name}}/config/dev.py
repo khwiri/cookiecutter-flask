59b8fc59061f91abd9b4a4cce9c65c707f8c0f62
khwiri <5530052+khwiri@users.noreply.github.com>
2022-03-04 22:35:04 -0500

    :wrench: Set SQLALCHEMY_TRACK_MODIFICATIONS to True in Template Dev Config
    This has been set to True so that deprecation warnings are not emitted. In
    the future None will become disabled which means Flask-SQLAlchemy will not
    track modifications of objects or emit signals by default which may be
    expected behavior.

--- {{cookiecutter.app_name}}/config/dev.py
+++ {{cookiecutter.app_name}}/config/dev.py
@@ -2,8 +2,9 @@
 
 # NOTE: Database connection strings for all other environments should be placed in instance/config.py
 import os
-APP_PASSWORD = os.environ['APP_PASSWORD']
+APP_PASSWORD      = os.environ['APP_PASSWORD']
 MIGRATOR_PASSWORD = os.environ['MIGRATOR_PASSWORD']
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{{ cookiecutter.app_name }}:{APP_PASSWORD}@{{ cookiecutter.app_name }}-postgres:5432/{{ cookiecutter.app_name }}'
+SQLALCHEMY_TRACK_MODIFICATIONS    = True
+SQLALCHEMY_DATABASE_URI           = f'postgresql://{{ cookiecutter.app_name }}:{APP_PASSWORD}@{{ cookiecutter.app_name }}-postgres:5432/{{ cookiecutter.app_name }}'
 SQLALCHEMY_DATABASE_MIGRATION_URI = f'postgresql://migrator:{MIGRATOR_PASSWORD}@{{ cookiecutter.app_name }}-postgres:5432/{{ cookiecutter.app_name }}'
