2c83d42748915b06a131981ffefedf193d6f3167
khwiri <5530052+khwiri@users.noreply.github.com>
2022-09-19 22:04:18 -0400

    Switch Integration Tests to Resource Requests

--- tests/integration.sh
+++ tests/integration.sh
@@ -4,92 +4,81 @@
 pushd "$(dirname "$(cd "$(dirname "$0")" && pwd)")" > /dev/null || exit 1
 
 compose_up()
-{
-    pushd output/app > /dev/null || exit 1
-    docker compose up --detach &> /dev/null
-    popd > /dev/null || exit 1
-}
+(
+    set -e
+    cd output/app &> /dev/null
+    docker compose up --build --detach &> /dev/null
+)
 
 compose_down()
-{
-    pushd output/app > /dev/null || exit 1
+(
+    set -e
+    cd output/app &> /dev/null
     docker compose down &> /dev/null
-    popd > /dev/null || exit 1
-}
-
-app_service_is_ready()
-{
-    pipenv run http --check-status GET :5000
-    return $?
-}
+)
 
 cleanup()
 {
     printf "\n\e[33mCleanup\e[32m...\e[0m"
-    compose_down
-    printf "\e[32mdone\e[0m\n\n"
+    compose_down || { printf "\e[31mfailed\e[0m\n"; return 1; }
+    printf "\e[32mok\e[0m\n\n"
 }
 
+is_resource_available()
+{
+    pipenv run http --check-status GET "http://127.0.0.1:5000${1}" &> /dev/null
+}
 
-printf "\n\e[33mTest Cookiecutter Build\e[32m...\e[0m"
-pipenv run build &> /dev/null
-EXIT_CODE=$?
-if [ "$EXIT_CODE" -ne 0 ]; then
-    printf "\e[31mfailed\e[0m\n"
+wait_for_resource()
+{
+    ATTEMPT=0
+    until is_resource_available "$1"
+    do
+        ATTEMPT=$((ATTEMPT + 1))
+        if [[ $((ATTEMPT)) -ge 60 ]]; then
+            return 1
+        else
+            printf "\e[32m.\e[0m"
+        fi
+        sleep 1;
+    done
+}
+
+on_error()
+{
     cleanup
     printf "\e[31mFAILED!\e[0m\n\n"
     exit 1
-else
-    printf "\e[32mpassed\e[0m\n"
-fi
+}
 
-printf "\e[33mTest Create and Start Containers\e[32m...\e[0m"
-compose_up
-ATTEMPT=0
-until app_service_is_ready
-do
-    ATTEMPT=$((ATTEMPT + 1))
-    if [[ $((ATTEMPT)) -ge 30 ]]; then
-        printf "\e[31mfailed\e[0m\n"
-        cleanup
-        printf "\e[31mFAILED!\e[0m\n\n"
-        exit 1
-    else
-        printf "\e[32m.\e[0m"
-    fi
+printf "\n\e[33mTest Cookiecutter Build\e[32m...\e[0m"
+pipenv run build &> /dev/null || { printf "\e[31mfailed\e[0m\n"; on_error; }
+printf "\e[32mok\e[0m\n"
 
-    sleep 1;
-done
-printf "\e[32mpassed\e[0m\n"
+printf "\e[33mTest Containers Up\e[32m...\e[0m"
+compose_up || { printf "\e[31mfailed\e[0m\n"; on_error; }
+printf "\e[32mok\e[0m\n"
 
-printf "\e[33mTest Static Files[32m...\e[0m"
-STATIC_FILES=(
-    "./output/app/app/static/css/main.min.css"
-    "./output/app/app/static/css/main.min.css.map"
-    "./output/app/app/static/js/main.min.js"
-    "./output/app/app/static/js/main.min.js.map"
-    "./output/app/app/static/img/pug1.jpg"
-    "./output/app/app/static/img/pug2.jpg"
+printf "\e[33mTest Resources\e[32m...\e[0m"
+RESOURCE_TESTS=(
+    "/"
+    "/static/css/main.min.css"
+    "/static/css/main.min.css.map"
+    "/static/js/main.min.js"
+    "/static/js/main.min.js.map"
+    "/static/img/pug1.jpg"
+    "/static/img/pug2.jpg"
 )
-for STATIC_FILE in "${STATIC_FILES[@]}"
+for RESOURCE_TEST in "${RESOURCE_TESTS[@]}"
 do
-    if [[ ! -f "$STATIC_FILE" ]]; then
-        printf "\e[31mfailed\e[0m\n"
-        printf "  \"%s\" does not exist\n" "$STATIC_FILE"
-        ls -l
-        ls -l ./output
-        ls -l ./output/app
-        ls -l ./output/app/app
-        ls -l ./output/app/app/static
-        ls -l ./output/app/app/static/css
-        cleanup
-        printf "\e[31mFAILED!\e[0m\n\n"
-        exit 1
-    else
-        printf "\e[32m.\e[0m"
-    fi
+    wait_for_resource "$RESOURCE_TEST" || {
+        printf "\e[31mfailed\e[0m\n";
+        printf "  \"%s\" does not exist\n" "$RESOURCE_TEST";
+        on_error;
+    }
+    printf "\e[32m.\e[0m"
 done
-printf "\e[32mpassed\e[0m\n"
+printf "\e[32mok\e[0m\n"
 
 cleanup
 
